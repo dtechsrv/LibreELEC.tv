7da39bb09e408823c434f528d964dff4b344e8da
SupervisedThinking <supervisedthinking@gmail.com>
2022-07-14 14:43:38 +0200

    retroarch-joypad-autoconfig: update to git f8fdb2f

--- packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/retroarch-joypad-autoconfig/package.mk
+++ packages/supervisedthinking/emulation/frontends/Retroarch/retroarch-depends/retroarch-joypad-autoconfig/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="1.10.3"
-PKG_SHA256="df4c240dde056fade9a4f1a505f3e7b8ffeaa464223b1fa19953ed7c19edce2f"
+PKG_VERSION="f8fdb2f45eaa334d282b46ff9f5fd2efc0856b13" # v 1.10.3+
+PKG_SHA256="3a30a5e6819fc3f70ba5aa92ede08d9079360dceee08db236decbc70010b3742"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/retroarch-joypad-autoconfig"
-PKG_URL="https://github.com/libretro/retroarch-joypad-autoconfig/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/libretro/retroarch-joypad-autoconfig/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="gcc:host"
 PKG_LONGDESC="RetroArch joypad autoconfig files"
 PKG_TOOLCHAIN="manual"
